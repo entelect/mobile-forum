3a7caf8cbd17f935e000ea2c00c44cb0d1616f30
Christopher Araujo <dcaraujo39@gmail.com>
2020-07-01 20:13:15 +0200

    Added Animate example

--- Android/ui-ux/app/src/main/java/za/co/entelect/kindling/MainActivity.kt
+++ Android/ui-ux/app/src/main/java/za/co/entelect/kindling/MainActivity.kt
@@ -1,12 +1,26 @@
 package za.co.entelect.kindling
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onSharedElementExampleTapped(view: View) {
+
+    }
+
+    fun onMotionLayoutExampleTapped(view: View) {
+
+    }
+
+    fun onAnimateOptionTapped(view: View) {
+        startActivity(Intent(this, AnimateExampleActivity::class.java))
     }
 }
