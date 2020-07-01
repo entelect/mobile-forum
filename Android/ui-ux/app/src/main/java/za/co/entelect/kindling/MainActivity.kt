c9550d316768cd7af57fc76e0781ffda13329df2
Christopher Araujo <dcaraujo39@gmail.com>
2020-07-01 18:34:16 +0200

    Added UI/UX project with 3 buttons

---
+++ Android/ui-ux/app/src/main/java/za/co/entelect/kindling/MainActivity.kt
@@ -0,0 +1,12 @@
+package za.co.entelect.kindling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
