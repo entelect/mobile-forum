2c959d2f0e2c8f6e12203ecc7d05f5cf947a0ac4
Christopher Araujo <dcaraujo39@gmail.com>
2020-07-05 12:24:30 +0200

    Enabled dark mode

--- Android/ui-ux/app/src/main/java/za/co/entelect/kindling/MainActivity.kt
+++ Android/ui-ux/app/src/main/java/za/co/entelect/kindling/MainActivity.kt
@@ -4,20 +4,36 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import za.co.entelect.kindling.animate.AnimateExampleActivity
 import za.co.entelect.kindling.motionlayout.MotionLayoutExampleActivity
-import za.co.entelect.kindling.sharedelement.SharedElementExampleActivity
 import za.co.entelect.kindling.transition.TransitionExampleActivity
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val DARK_MODE_ENABLED = "DARK_MODE_ENABLED"
+    }
+
+    private var darkModeEnabled: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        darkModeEnabled = wasDarkModeEnabled(savedInstanceState)
+        updateDarkModeState()
     }
 
-    fun onSharedElementExampleTapped(view: View) {
-        startActivity(Intent(this, SharedElementExampleActivity::class.java))
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(DARK_MODE_ENABLED, darkModeEnabled)
+    }
+
+    fun onDarkThemeSelected(view: View) {
+        darkModeEnabled = !darkModeEnabled
+        updateDarkModeState()
     }
 
     fun onMotionLayoutExampleTapped(view: View) {
@@ -31,4 +47,11 @@ class MainActivity : AppCompatActivity() {
     fun onTransitionExampleTapped(view: View) {
         startActivity(Intent(this, TransitionExampleActivity::class.java))
     }
+
+    private fun updateDarkModeState() {
+        AppCompatDelegate.setDefaultNightMode(if (darkModeEnabled) MODE_NIGHT_YES else MODE_NIGHT_NO)
+    }
+
+    private fun wasDarkModeEnabled(savedInstanceState: Bundle?) =
+        savedInstanceState?.getBoolean(DARK_MODE_ENABLED, false) ?: false
 }
