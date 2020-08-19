fe807a9a72b9e888f185a64bb59c13173adcd901
Christopher Araujo <christopher.araujo@hey.com>
2020-08-19 16:11:02 +0200

    Added iOS project for CI/CD

---
+++ iOS/ios-ci-cd/Kindling-Cocoapods/Kindling-Cocoapods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kindling-Cocoapods
+//
+//  Created by Christopher Araujo on 2020/08/19.
+//  Copyright © 2020 Entelect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
