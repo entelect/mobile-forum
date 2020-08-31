55ea3ba93e113b38f6d86150fd485ef0de87ee01
Christopher Araujo <christopher.araujo@hey.com>
2020-08-31 13:11:55 +0200

    Moved CI/CD to root of repo

--- Android/Android-fundamentals/KindlingApp/app/src/main/java/za/co/entelect/mobile/forum/kindlingapp/ui/profile/ProfileViewModel.java
+++
@@ -1,7 +0,0 @@
-package za.co.entelect.mobile.forum.kindlingapp.ui.profile;
-
-import androidx.lifecycle.ViewModel;
-
-public class ProfileViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- Android/Reactive-programming/Coroutines-advanced-part-3/app/src/main/java/za/co/knuckles/avancedcoroutines/common/Result.kt
+++
@@ -1,3 +0,0 @@
-package za.co.knuckles.avancedcoroutines.common
-
-class Result<out T>(val value:T?, val throwable: Throwable?)
