38522709dd23963a5b1449eb021f300b46f55a40
Armand Kamffer <armand.kamffer@entelect.co.za>
2020-09-07 09:21:40 +0200

    Add basic Obj-C to Swift project

---
+++ iOS/iOS-advanced/Obj-C/Obj-C/ViewControllers/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  Obj-C
+//
+//  Created by Armand Kamffer on 2020/09/07.
+//  Copyright © 2020 Armand Kamffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
